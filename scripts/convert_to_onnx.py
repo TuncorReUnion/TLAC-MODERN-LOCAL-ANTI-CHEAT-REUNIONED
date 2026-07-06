03ce8c48d504c3a3307beef198bdcdabaa8ae037
TuncorReUnion <tuncor865@gmail.com>
2026-07-06 16:27:27 +0300

    Enhance ONNX conversion with target opset and options
    Added target opset and options for ONNX conversion.

--- scripts/convert_to_onnx.py
+++ scripts/convert_to_onnx.py
@@ -7,7 +7,12 @@ model = joblib.load('anomaly_model.pkl')
 
 initial_type = [('float_input', FloatTensorType([None, 3]))]
 
-onnx_model = convert_sklearn(model, initial_types=initial_type)
+onnx_model = convert_sklearn(
+    model,
+    initial_types=initial_type,
+    target_opset=12,
+    options={id(model): {'zipmap': False}}
+)
 
 with open("anomaly_model.onnx", "wb") as f:
     f.write(onnx_model.SerializeToString())
