66c98ca7463f313433eb19cba1e7ca73a986b8b6
TuncorReUnion <tuncor865@gmail.com>
2026-07-07 15:45:52 +0300

    Refactor server_main to use new server module

--- src/bin/server_main.rs
+++ src/bin/server_main.rs
@@ -1,9 +1,8 @@
-use anti_cheat::server::{run_server, ServerState};
-use std::sync::Arc;
+mod server;
+
+use server::{run_server, ServerState};
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    env_logger::init();
-    let state = Arc::new(ServerState::new());
-    run_server("/tmp/anti-cheat.sock", state).await
+    run_server().await
 }
